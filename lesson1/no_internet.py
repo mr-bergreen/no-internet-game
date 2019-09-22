2bdec7602c3dec384beea8b9b55e4503c6adda7a
Keith Bergreen <36710853+mr-bergreen@users.noreply.github.com>
2019-09-22 10:28:57 -0400

    Added lesson 1 folder

---
+++ lesson1/no_internet.py
@@ -0,0 +1,38 @@
+# no-internet-game - lesson 1
+
+import pygame
+from pygame.locals import *
+import sys
+
+#------------ INIT ------------#
+pygame.init()
+
+WIDTH = 480
+HEIGHT = 480
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+clock = pygame.time.Clock()
+
+#------------ COLORS ------------#
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+#------------ Game Loop ------------#
+while True:
+  
+  # Event Loop
+  for event in pygame.event.get():
+    if event.type == QUIT:
+      pygame.quit()
+      sys.exit()
+
+  # Update
+
+  # Draw
+  screen.fill(BLACK)
+  
+  pygame.draw.line(screen, WHITE, (0,HEIGHT/2), (WIDTH,HEIGHT/2))
+
+  # Load
+  pygame.display.flip()
+  clock.tick(60)
